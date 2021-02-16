a1250b45100f65981ebc032a28f15566105995d7
Mariam <mariamnozadze97@yahoo.com>
2021-02-16 19:10:14 +0400

    Made some changes

---
+++ gsap.js
@@ -0,0 +1,3 @@
+gsap.from(".logo", { opacity: 0, duration: 1, delay: 0.5, y: -10 });
+gsap.from(".name", { opacity: 0, duration: 1, delay: 1.0, x: -200 });
+gsap.from(".social", { opacity: 0, duration: 1, delay: 2, y: -50 });
